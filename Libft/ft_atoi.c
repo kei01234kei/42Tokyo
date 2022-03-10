6497dbe28a1fb936bd0c4115948be1b5baa6edc2
Keisuke Ishigami <keisuke.130@icloud.com>
2022-03-10 17:02:41 +0900

    atoi関数を改善

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,54 +6,35 @@
 /*   By: keisuke <keisuke.130@icloud.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:26:37 by keisuke           #+#    #+#             */
-/*   Updated: 2022/03/10 09:12:47 by keisuke          ###   ########.fr       */
+/*   Updated: 2022/03/10 16:59:53 by keisuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static long	add_safety(long a, long b)
-{
-	if ((a > 0) && (b > (LONG_MAX - a)))
-		return (LONG_MAX);
-	if ((a < 0) && (b < (LONG_MIN - a)))
-		return (LONG_MIN);
-	return (a + b);
-}
-
-static long	mul_safety(long a, long b)
-{
-	if ((a > 0) && (a > (LONG_MAX / b)))
-		return (LONG_MAX);
-	if ((a < 0) && (a < (LONG_MIN / b)))
-		return (LONG_MIN);
-	return (a * b);
-}
-
 int	ft_atoi(const char *str)
 {
-	size_t	i;
-	int		sign;
-	long	num;
+	long int	res;
+	int			num;
 
-	i = 0;
-	sign = 1;
-	num = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
+	res = 0;
+	num = 1;
+	while (('\t' <= *str && *str <= '\r') || (*str == ' '))
+		str++;
+	if (*str == '+' || *str == '-')
 	{
-		num = mul_safety(num, 10);
-		num = add_safety(num, (str[i++] - '0') * sign);
-		if (sign == 1 && num == LONG_MAX)
-			return ((int)LONG_MAX);
-		if (sign == -1 && num == LONG_MIN)
-			return ((int)LONG_MIN);
+		if (*str == '-')
+			num *= -1;
+		str++;
 	}
-	return (num);
+	while ('0' <= *str && *str <= '9')
+	{
+		res = res * 10 + *str - '0';
+		str++;
+		if (res > (long int)INT_MIN * -1 && num == -1)
+			return (0);
+		if (res > INT_MAX && num == 1)
+			return (-1);
+	}
+	return ((int)res * num);
 }
