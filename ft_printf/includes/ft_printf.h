18c82728396104ebca69ab752e3e71d73fd49126
Keisuke Ishigami <keisuke.130@icloud.com>
2022-06-11 11:59:56 +0900

    ヘッダファイルを作成

---
+++ ft_printf/includes/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kishigam <kishigam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 11:10:28 by kishigam          #+#    #+#             */
+/*   Updated: 2022/06/11 11:59:10 by kishigam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+#endif
