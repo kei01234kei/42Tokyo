337dca7a9dac1e3c6019169ff2efbfe59c5bda06
Keisuke Ishigami <keisuke.130@icloud.com>
2022-06-11 15:15:24 +0900

    libftのディレクトリを修正

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -6,16 +6,19 @@
 /*   By: kishigam <kishigam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 11:10:28 by kishigam          #+#    #+#             */
-/*   Updated: 2022/06/11 11:59:10 by kishigam         ###   ########.fr       */
+/*   Updated: 2022/06/11 14:49:39 by kishigam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
-# include <stdlib.h>
+# include "../libft/libft.h"
 # include <unistd.h>
 # include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_parse(char c, va_list format);
+int	ft_putchar(char c);
 
 #endif
