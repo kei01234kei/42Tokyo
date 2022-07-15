f25fc22a50787f21e7d3c217fefb0804e3b5d051
Keisuke Ishigami <keisuke.130@icloud.com>
2022-07-15 14:45:19 +0900

    s1かs2のどちらか一方がNULLのときNULLを返してもらう問題を修正

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: keisuke <keisuke.130@icloud.com>           +#+  +:+       +#+        */
+/*   By: kishigam <kishigam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 17:32:40 by keisuke           #+#    #+#             */
-/*   Updated: 2022/03/09 11:17:57 by keisuke          ###   ########.fr       */
+/*   Updated: 2022/07/15 14:44:02 by kishigam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	int		i;
+	int		j;
 	char	*str;
-	size_t	i;
-	size_t	j;
 
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
+	i = -1;
+	j = -1;
+	if (!s1 && !s2)
 		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
-	while (s1[i])
-	{
+	while (s1[++i])
 		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
+	while (s2[++j])
+		str[i++] = s2[j];
 	str[i] = '\0';
+	free((char *)s1);
 	return (str);
 }
