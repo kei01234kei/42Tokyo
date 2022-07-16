b4bf239c9032d66059f05fa115905510c5e41120
Keisuke Ishigami <keisuke.130@icloud.com>
2022-07-17 08:14:48 +0900

    main関数の書き方が間違っていたため更新

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,28 +6,34 @@
 /*   By: kishigam <kishigam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 11:22:35 by kishigam          #+#    #+#             */
-/*   Updated: 2022/07/12 12:33:34 by kishigam         ###   ########.fr       */
+/*   Updated: 2022/07/17 08:14:18 by kishigam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main(void)
+int main(void)
 {
-	int		fd;
-	int		rc;
-	char	*buf;
+    int fd;
+    char *s;
 
-	buf = (char *)malloc(sizeof(char) * 100);
 	fd = open("./text.txt", O_RDONLY);
-	// rc = read(fd, buf, 100);
-	// printf("%d文字読む(改行文字含む)\n", rc);
-	// printf("%s\n", buf);
-	// rc = read(fd, buf, 100);
-	// printf("%d\n", rc);
-	// printf("%s\n", buf);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	close(fd);
+
+	s = get_next_line(fd);
+    printf("%s", s);
+    free(s);
+    s = get_next_line(fd);
+    printf("%s", s);
+    free(s);
+    s = get_next_line(fd);
+    printf("%s", s);
+    free(s);
+    s = get_next_line(fd);
+    printf("%s", s);
+    free(s);
+    s = get_next_line(fd);
+    printf("%s", s);
+    free(s);
+    close(fd);
+    system("leaks a.out");
 }
